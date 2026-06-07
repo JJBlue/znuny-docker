1c34c5f0f2bef02fd5efcb1a31b7e91ec6149fe1
JJBlue <JJBlue@SMITH>
2026-06-07 11:17:08 +0200

    Fix Setup config.pm

--- src/entrypoints/apache.sh
+++ src/entrypoints/apache.sh
@@ -10,21 +10,9 @@ bash "$SCRIPT_DIR/znuny.sh"
 # You can do some boot checks, like initial setup steps here.
 # Vars
 external_port="${external_port:-80}"
-PERSISTENT_CFG="/persistent/Config.pm"
-ZNUNY_CFG="/opt/znuny/Kernel/Config.pm"
 
 # Welcome message
 echo Listening on port ${external_port}
-
-
-# Setup persistence
-if [ -f "$PERSISTENT_CFG" ]; then
-    echo "Using persistent Config.pm"
-else
-    echo "Persistent Config.pm not found, seeding it"
-    cp "$ZNUNY_CFG" "$PERSISTENT_CFG"
-fi
-ln -sf "$PERSISTENT_CFG" "$ZNUNY_CFG"
 
 # Move to Main process
 exec gosu www-data "$@"
--- src/entrypoints/znuny.sh
+++ src/entrypoints/znuny.sh
@@ -1,22 +1,42 @@
 # Stop on Error
 set -e
 
-# TODO run once
-
 # TODO own Downgrade: FAIL --> Change Version File manually to continue
 
-# TODO Get ${VERSION} from VERSION file in persistent folder
-VERSION=latest
-FILE_RELEASE=${ZNUNY_HOME}/RELEASE
-FILE_KERNEL=${ZNUNY_HOME}/KERNEL
-
-# Create Folder
+# Create Folder # TODO once
 chown -R www-data:www-data /persistent
 
+PERSISTENT_CFG="/persistent/Config.pm"
+ZNUNY_HOME="/opt/znuny"
+ZNUNY_CFG="${ZNUNY_HOME}/Kernel/Config.pm"
+DB_HOST="${DB_HOST:-127.0.0.1}"
+DB_NAME="${DB_NAME:-znuny}"
+DB_USER="${DB_USER:-znuny}"
+DB_USER_PASS="${DB_USER_PASS:-some-pass}"
+
 # Create Config.pm if it doesn't already exist
-if [ ! -f /persistent/Config.pm ]; then \
-    cp /opt/znuny/Kernel/Config.pm.dist /persistent/Config.pm; \
+if [ ! -f "$PERSISTENT_CFG" ]; then
+    echo "Persistent Config.pm not found, seeding it"
+    cp /opt/znuny/Kernel/Config.pm.dist "$PERSISTENT_CFG"
+
+    sed -i "s/\(\$Self->{DatabaseHost}\s*=\s*\)'[^']*';/\1'$DB_HOST';/" "$PERSISTENT_CFG"
+    sed -i "s/\(\$Self->{Database}\s*=\s*\)'[^']*';/\1'$DB_NAME';/"     "$PERSISTENT_CFG"
+    sed -i "s/\(\$Self->{DatabaseUser}\s*=\s*\)'[^']*';/\1'$DB_USER';/" "$PERSISTENT_CFG"
+    sed -i "s/\(\$Self->{DatabasePw}\s*=\s*\)'[^']*';/\1'$DB_USER_PASS';/" "$PERSISTENT_CFG"
+
+    chown -R znuny:www-data "$PERSISTENT_CFG"
+    chmod -R 770 "$PERSISTENT_CFG"
+else
+    echo "Using persistent Config.pm"
 fi
-ln -s /persistent/Config.pm Kernel/Config.pm
+
+# Create Config.pm symlink if not exist
+if [ ! -L ${ZNUNY_HOME}/Kernel/Config.pm ]; then
+    ln -sf "$PERSISTENT_CFG" "$ZNUNY_CFG"
+    $ZNUNY_HOME/bin/znuny.SetPermissions.pl
+fi
+
+echo "For initial setup, visit http://hostname:port/znuny/installer.pl"
+
 
 # TODO Upgrade / Update Command: Only if Version changed
